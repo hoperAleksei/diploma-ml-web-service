be5f9f582e6dde3be15d1266ce3ad4c079c024ed
Rebarial <90740397+Rebarial@users.noreply.github.com>
2023-07-11 23:41:40 +1000

    fix

--- server/preprocessing/analytic_function.py
+++ server/preprocessing/analytic_function.py
@@ -4,19 +4,39 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def class_count(label):
-    label.value_counts().to_dict()
+def class_count(label: pd.Series) -> dict:
+    """"
+    :param label: Series с label
+    :return: dict (key - название класса, value - колиечство)
+    """
+    return label.value_counts().to_dict()
 
-def atribute_distribution(atribute):
+def atribute_distribution(atribute: pd.Series) -> plt.hist:
+    """"
+    :param atribute: Series с параметром
+    :return: plt.hist
+    """
     return  plt.hist(atribute, color='blue', edgecolor='black', bins=int(180 / 5))
 
-def correlation_hotmap(data):
+def correlation_hotmap(data: pd.DataFrame)  -> sns.heatmap:
+    """"
+    :param data: DataFrame
+    :return: dsns.heatmap
+    """
     correlation_matrix = data.corr()
     plt.figure(figsize = (10, 6))
-    return (sns.heatmap(correlation_matrix, annot = True))
+    return sns.heatmap(correlation_matrix, annot = True)
 
-def miss_values_count(data):
+def miss_values_count(data: pd.DataFrame) -> dict:
+    """"
+    :param data: DataFrame
+    :return: dsns.heatmap
+    """
     return data.isnull().sum().to_dict()
 
-def data_types(data):
+def data_types(data: pd.DataFrame) -> dict:
+    """"
+    :param data: DataFrame
+    :return: dsns.heatmap
+    """
     return data.dtypes.to_dict()
